7038001ae3d27cfdb705200380b523551c5772b8
Dawid Fieluba <c_dawid.fieluba@light.co>
2021-07-31 16:38:25 +0200

    Supporting multiple block definitions in task definition file

--- tmetrics_wrapper/src/input_parser.py
+++ tmetrics_wrapper/src/input_parser.py
@@ -13,38 +13,38 @@ class TasksParser(object):
         self.config: {} = config
 
     def parse(self, tasks_definition: str) -> (date, date, [Task]):
-        with open(file=tasks_definition, mode='r') as file:
-            lines = [line.rstrip() for line in file.readlines()]
-            lines = [line for line in lines if line]
+        LOG.debug(f'Parsing task definition:\n{tasks_definition}')
+        lines = [line.rstrip() for line in tasks_definition.splitlines()]
+        lines = [line for line in lines if line]
 
-            date_range = lines[0].split('-')
-            start_date = datetime.strptime(date_range[0], '%d.%m.%Y').date()
-            end_date = datetime.strptime(date_range[1], '%d.%m.%Y').date()
+        date_range = lines[0].split('-')
+        start_date = datetime.strptime(date_range[0], '%d.%m.%Y').date()
+        end_date = datetime.strptime(date_range[1], '%d.%m.%Y').date()
 
-            LOG.debug(f'Parsed start date: {start_date}')
-            LOG.debug(f'Parsed end date: {end_date}')
+        LOG.debug(f'Parsed start date: {start_date}')
+        LOG.debug(f'Parsed end date: {end_date}')
 
-            task_list: [Task] = []
-            for line in lines[1:]:
-                LOG.debug(f'Parsing line: {line}')
-                split_line = line.split('|')
-                hours = int(split_line[0].split(':')[0])
-                try:
-                    minutes = int(split_line[0].split(':')[1])
-                except IndexError:
-                    minutes = 0
-                note = split_line[1]
-                project = split_line[2]
-                try:
-                    requested_split = int(split_line[3])
-                except IndexError:
-                    requested_split = 1
-                task = Task(note, self._get_project_id(project), duration=timedelta(hours=hours, minutes=minutes),
-                            requested_split=requested_split)
-                LOG.debug(f'Line parsed to task: {task}')
-                task_list.append(task)
-            LOG.info('Task file parsed.')
-            return start_date, end_date, task_list
+        task_list: [Task] = []
+        for line in lines[1:]:
+            LOG.debug(f'Parsing line: {line}')
+            split_line = line.split('|')
+            hours = int(split_line[0].split(':')[0])
+            try:
+                minutes = int(split_line[0].split(':')[1])
+            except IndexError:
+                minutes = 0
+            note = split_line[1]
+            project = split_line[2]
+            try:
+                requested_split = int(split_line[3])
+            except IndexError:
+                requested_split = 1
+            task = Task(note, self._get_project_id(project), duration=timedelta(hours=hours, minutes=minutes),
+                        requested_split=requested_split)
+            LOG.debug(f'Line parsed to task: {task}')
+            task_list.append(task)
+        LOG.info('Task file parsed.')
+        return start_date, end_date, task_list
 
     def _get_project_id(self, project) -> int:
         LOG.debug(f'Getting project id for {project}')
--- tmetrics_wrapper/tmetrics_wrapper.py
+++ tmetrics_wrapper/tmetrics_wrapper.py
@@ -12,10 +12,7 @@ from src.utils import config_logger
 
 LOG = logging.getLogger(__name__)
 
-DATE_FORMAT = '%d.%m.%Y'
-
-start_date = datetime.datetime.strptime('12.07.2021', DATE_FORMAT)
-end_date = datetime.datetime.strptime('16.07.2021', DATE_FORMAT)
+TASK_DEFINITION_SEPARATOR = '---'
 
 _shared_options = [
     click.option('--account-id', default=os.environ.get('TMETRICS_ACCOUNT_ID'), required=True,
@@ -37,39 +34,61 @@ def add_options(options):
 
 
 @click.group()
-@click.option('-v', '--verbose', is_flag=True, default=False, help='Enable debug logs.')
-def cli(verbose):
-    config_logger(verbose=verbose)
+def cli():
+    pass
 
 
 @cli.command()
 @add_options(_shared_options)
-def list_projects(account_id, user_token, host, dry_run):
+@click.option('--out-file', type=click.Path(exists=True), required=True)
+def init_config(verbose, account_id, user_token, host, out_file, dry_run, assume_yes):
+    config_logger(verbose)
     LOG.debug(f'Listing projects for account id: {account_id}')
     api = TMetricsAPI(account_id=account_id, token=user_token, host=host)
-    print(api.get_projects())
+    if dry_run:
+        LOG.error("Cannot generate config with dry run option.")
+        exit(0)
+    project_list = api.get_projects()
+    config_projects = {}
+    for project in project_list:
+        client_name = project.get("client").get("name")
+        project_name = project.get("name")
+        project_id = project.get("id")
+        key = f'{project_name}/{client_name}'
+        config_projects[key] = {"id": project_id, "alias": ""}
+    config = {"projects": config_projects}
+    LOG.info(f'Generated config:\n{config}')
+    if query_yes_no(f'Wrtie to file? ({out_file})'):
+        with open(out_file, 'w', encoding='utf-8') as file:
+            LOG.info(f'Writing config to file: {out_file}')
+            json.dump(config, file)
 
 
 @cli.command()
 @add_options(_shared_options)
 @click.option('--tasks-file', type=click.Path(exists=True), required=True)
 @click.option('--config-file', type=click.Path(exists=True), required=True)
-def run(account_id, user_token, host, tasks_file, config_file, dry_run):
+def run(verbose, account_id, user_token, host, tasks_file, config_file, dry_run, assume_yes):
+    config_logger(verbose=verbose)
     LOG.debug(f'Running for account id: {account_id} on host: {host}')
     api = TMetricsAPI(account_id=account_id, token=user_token, host=host)
     with open(file=config_file, mode='r') as config_file:
         config = json.load(config_file)
+    with open(file=tasks_file, mode='r') as tasks_file:
+        tasks_definition_list = tasks_file.read().split(TASK_DEFINITION_SEPARATOR)
     parser = TasksParser(config)
-    start_date, end_date, task_list = parser.parse(tasks_file)
-    planner = TimeEntryPlanner(start_date, end_date, task_list)
-    planner.plan()
-    planner.display_current_plan()
-
-    for workday in planner.workday_list:
-        for task in workday.task_list:
-            if not dry_run:
-                api.add_task_time_entry(task)
-                LOG.info(f'Pushed time entries for {workday}')
+    LOG.debug(f'Task definitions{tasks_definition_list}')
+    for task_definition in tasks_definition_list:
+        start_date, end_date, task_list = parser.parse(task_definition)
+        planner = TimeEntryPlanner(start_date, end_date, task_list)
+        planner.plan()
+        planner.display_current_plan()
+        if assume_yes or query_yes_no(question="Are you sure?"):
+            for workday in planner.workday_list:
+                for task in workday.task_list:
+                    if not dry_run:
+                        api.add_task_time_entry(task)
+                        LOG.info(f'Pushed time entries for {workday}')
 
 
 if __name__ == "__main__":
